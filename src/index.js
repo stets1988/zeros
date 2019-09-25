bfd58d99d18ab28370a8a82055eec25719056463
stets1988 <stetsiuk.pavel@gmail.com>
2019-09-26 00:37:52 +0300

    init: start task zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function zeros(expression) {
-  // your solution
+  function zeros(expression) {
+  	var fact_summ=1;
+  	var zero=0;
+  	var length=expression.length;
+  	var fact=expression.slice(0,length-1);
+  	for(var i=1; i<=fact; i++)
+  	{ 
+  		fact_summ*=i;
+
+  	}
+  	fact_summ_string=String(fact_summ);
+  	for(var j=0; j<fact_summ_string.length; j++)
+  	{
+  		if(fact_summ_string[j]==0)
+  			zero+=1;
+  	}
+  	return zero;
+}// your solution
 }
