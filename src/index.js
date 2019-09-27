a805c9d37247692fea68bd984c05764226dadd9f
stets1988 <stetsiuk.pavel@gmail.com>
2019-09-28 00:09:52 +0300

    fix:  bug arr3

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 module.exports = function zeros(expression) {
   	let fact_summ=1;
-  	let zero= 0;
   	let count=0;
   	let k=0;
   	let p=0;
-  	let del=0;
   	let check="";
   	let arr2 = []; //считается через 2 числа
   	let arr3 = []; //обычный факториал
-  	let arr5 = []; 
   	let arr = expression.split('*');	
 
   	arr.forEach((item) => {
@@ -17,7 +14,6 @@ module.exports = function zeros(expression) {
   		check=`${item}`[k-1]+`${item}`[k-2];
   		
   	}
-  	// alert(check);
   	if(check == "!!" )
   		{
   			p=`${item}`.slice(0, k-2);
@@ -30,6 +26,24 @@ module.exports = function zeros(expression) {
   		}
 });
 
+ 	arr3.forEach((item) => {
+  		for(let m=1; m<=`${item}`;m++)
+  			{
+		  		fact_summ*=m;
+  		for (var k = 0; k<=fact_summ; k++)
+    		{	
+    			if(fact_summ%10==0)
+    			{
+    				count++;
+    				fact_summ/=10;
+    			}
+  			}
+  			}
+fact_summ=1;
+});
+
+
+
  	arr2.forEach((item) => {
   		if(`${item}`%2==0)
   		{
@@ -37,9 +51,6 @@ module.exports = function zeros(expression) {
   			{
 		  		fact_summ*=m;
 		  		m=m=2;
-		  		//alert(fact_summ);
-	  		
-			
   		for (var k = 0; k<=fact_summ; k++)
     		{	
     			if(fact_summ%10==0)
@@ -55,9 +66,6 @@ module.exports = function zeros(expression) {
   				for(let m=1; m<=`${item}`;m++)
   			{
 		  		fact_summ*=m;
-		  		//alert(fact_summ);
-	  		
-			
   		for (var k = 0; k<=fact_summ; k++)
     		{	
     			if(fact_summ%10==0)
